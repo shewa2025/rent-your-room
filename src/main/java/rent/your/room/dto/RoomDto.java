0061af78e067fe7a6e60fecfd8347e1a5e69e8e6
shewangzaw gebeyehu <nags2007@yahoo.com>
2025-11-27 16:51:36 -0500

    feat: Update RoomDto with User relationship

--- src/main/java/rent/your/room/dto/RoomDto.java
+++ src/main/java/rent/your/room/dto/RoomDto.java
@@ -1,19 +1,20 @@
 package rent.your.room.dto;
 
 import lombok.Data;
-import java.math.BigDecimal;
-import java.util.Set;
+import rent.your.room.model.Address;
+import rent.your.room.model.Amenity;
+import rent.your.room.model.User;
+
+import java.util.List;
 
 @Data
 public class RoomDto {
     private Long id;
-    private String title;
-    private String roomType;
+    private String name;
     private String description;
-    private BigDecimal rent;
-    private String imageUrl;
-    private boolean approved;
-    private AddressDto address;
-    private Set<AmenityDto> amenities;
-    private String ownerUsername;
+    private int price;
+    private String type;
+    private Address address;
+    private List<Amenity> amenities;
+    private User user;
 }
