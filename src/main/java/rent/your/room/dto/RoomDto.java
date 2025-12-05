a603893f32d7c6e24a49de57bc6fed9477edbd8a
shewangzaw gebeyehu <nags2007@yahoo.com>
2025-12-04 19:10:30 -0500

    fix: Resolve compilation errors by updating DTOs

--- src/main/java/rent/your/room/dto/RoomDto.java
+++ src/main/java/rent/your/room/dto/RoomDto.java
@@ -1,20 +1,19 @@
 package rent.your.room.dto;
 
 import lombok.Data;
-import rent.your.room.model.Address;
-import rent.your.room.model.Amenity;
-import rent.your.room.model.User;
 
-import java.util.List;
+import java.math.BigDecimal;
+import java.util.Set;
 
 @Data
 public class RoomDto {
     private Long id;
-    private String name;
+    private String title;
     private String description;
-    private int price;
-    private String type;
-    private Address address;
-    private List<Amenity> amenities;
-    private User user;
+    private BigDecimal rent;
+    private String imageUrl;
+    private boolean approved;
+    private String ownerUsername;
+    private AddressDto address;
+    private Set<AmenityDto> amenities;
 }
